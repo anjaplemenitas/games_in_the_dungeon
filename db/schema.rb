e09ca3c842e79d053c6be74b7f99816dcac8155f
Anja Plemenitas <anja.plemenitas@gmail.com>
2022-02-21 15:36:48 +0000

    creating model booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_153348) do
+ActiveRecord::Schema.define(version: 2022_02_21_153612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 2022_02_21_153348) do
     t.index ["user_id"], name: "index_boardgames_on_user_id"
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.boolean "buy"
+    t.boolean "rent"
+    t.bigint "user_id", null: false
+    t.bigint "boardgame_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["boardgame_id"], name: "index_bookings_on_boardgame_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -39,4 +50,6 @@ ActiveRecord::Schema.define(version: 2022_02_21_153348) do
   end
 
   add_foreign_key "boardgames", "users"
+  add_foreign_key "bookings", "boardgames"
+  add_foreign_key "bookings", "users"
 end

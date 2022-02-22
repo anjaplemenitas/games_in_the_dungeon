4f214c8fdb9a8ef27736d0081617801a545f8fe4
Anja Plemenitas <anja.plemenitas@gmail.com>
2022-02-22 11:27:17 +0000

    conflict resolved

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-<<<<<<< HEAD
-  has_many :boardgames, through: :bookings
-=======
 
   has_many :boardgames
->>>>>>> c5ff4b64df627f4c1338cd512aaef1ee30af0a88
 end
